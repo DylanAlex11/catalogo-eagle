53755efffaebf3d21bbfc6bfefd805f316ef64b2
DylanAlex11 <dylancabezas2005@gmail.com>
2026-03-17 16:32:14 -0600

    arreglando supabase y productos

--- services/supabase.js
+++ services/supabase.js
@@ -1,8 +1,9 @@
-const SUPABASE_URL = "https://pomgdjmvymkscpocpayu.supabase.co"
+const SUPABASE_URL = "https://pomgdjmvymkscpocpayu.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_9OazORM3uCHjS5r-BFU9eA_a_fAAChs"
+const SUPABASE_KEY = "sb_publishable_9OazORM3uCHjS5r-BFU9eA_a_fAAChs";
 
+// 👇 MUY IMPORTANTE: usar window.supabase
 const supabase = window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
-)
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
