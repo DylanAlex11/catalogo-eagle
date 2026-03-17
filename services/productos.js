db9003ca1e7db18f28be4ede08f6fe5870319b82
DylanAlex11 <dylancabezas2005@gmail.com>
2026-03-17 16:49:43 -0600

    Update productos.js

--- services/productos.js
+++ services/productos.js
@@ -1,17 +1,4 @@
-async function obtenerProductos(){
 
-const { data, error } = await supabase
-.from("productos")
-.select("*")
-
-if(error){
-console.error("Error cargando productos:", error)
-return []
-}
-
-return data
-
-}
 async function obtenerProductos() {
   try {
 
