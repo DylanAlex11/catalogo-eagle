bc008ccb6d5e76473b3854816501fe45c1be8ee9
DylanAlex11 <dylancabezas2005@gmail.com>
2026-03-17 17:43:06 -0600

    cambios html y supabase

--- services/supabase.js
+++ services/supabase.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://pomgdjmvymkscpocpayu.supabase.co";
 const SUPABASE_KEY = "sb_publishable_9OazORM3uCHjS5r-BFU9eA_a_fAAChs";
 
-// 👇 ESTA ES LA FORMA CORRECTA EN CDN V2
-const { createClient } = supabase;
-
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+// 👇 forma segura para CDN
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
